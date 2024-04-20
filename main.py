798e3e18354642cbb2a6b8f699df6295b5ede33a
ErRohitM <145048714+ErRohitM@users.noreply.github.com>
2024-04-20 13:59:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from os import getenv
 
 if __name__ == '__main__':
     port = int(getenv("PORT", 8000))
-    uvicorn.run("app.app:app", host="10.0.2.2", port = port, reload=True )
+    uvicorn.run("app.app:app", host="127.0.0.1", port = port, reload=True )
